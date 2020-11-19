f08d0881f933db2d03874824fb0458b15f8a567c
YanaTrifonova <trifonova.yana.prof@gmail.com>
2020-11-19 20:03:36 +0100

    added search input and button to nav bar

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,7 +1,20 @@
-import React from "react";
-import {Nav, Navbar} from 'react-bootstrap';
+import React, {useState} from "react";
+import {Form, FormControl, Nav, Navbar} from 'react-bootstrap';
+import Button from "react-bootstrap/Button";
 
 export default function NavigationBar() {
+    const [inputText, setInputText] = useState("");
+
+    const onClickHandler = (e) => {
+        e.preventDefault();
+
+        console.log(inputText);
+    }
+
+    const onChangeHandler = (event) => {
+        setInputText(event.target.value);
+    }
+
     return (
         <Navbar bg="light" expand="lg">
             <Navbar.Brand href="#home">Cocktails 🍸</Navbar.Brand>
@@ -12,6 +25,10 @@ export default function NavigationBar() {
                     <Nav.Link href="/discover">Discover Cocktails</Nav.Link>
                     <Nav.Link href="/about">About us</Nav.Link>
                 </Nav>
+                <Form inline onSubmit={onClickHandler}>
+                    <FormControl type="text" placeholder="Search" className="mr-sm-2" value={inputText} onChange={onChangeHandler}/>
+                    <Button variant="outline-success" type="submit">Search</Button>
+                </Form>
             </Navbar.Collapse>
         </Navbar>
     );
